7f7ff75850fdeeadf61cf5e8413d5249a5212b5c
Krstic Milan <milance@pop-os.localdomain>
2020-08-26 16:46:50 +0200

    Adding Routs

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-header",
@@ -6,11 +6,8 @@ import { Component, OnInit, EventEmitter, Output } from "@angular/core";
   styleUrls: ["./header.component.sass"],
 })
 export class HeaderComponent implements OnInit {
-  @Output() tabSelected = new EventEmitter<string>();
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {}
-  onSelect(activeTab: string) {
-    this.tabSelected.emit(activeTab);
-  }
+  ngOnInit() { }
+
 }
